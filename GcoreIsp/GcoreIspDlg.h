b0cb50cb02e86800b35b7882ce83e5fd68f3e98f
gagovski <gagovski@163.com>
2017-12-28 18:03:18 +0800

    first commit

---
+++ GcoreIsp/GcoreIspDlg.h
@@ -0,0 +1,33 @@
+
+// GcoreIspDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CGcoreIspDlg 对话框
+class CGcoreIspDlg : public CDialogEx
+{
+// 构造
+public:
+	CGcoreIspDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_GCOREISP_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	BOOL RegisterListenUSB(void);
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
